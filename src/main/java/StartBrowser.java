04f0b7568cec3b8fa1bbcf4e2a5aba5534f60d56
Igor Kozhin <imkozhin@ld.yandex.ru>
2016-08-25 12:53:10 +0300

    secondTest

--- src/main/java/StartBrowser.java
+++ src/main/java/StartBrowser.java
@@ -1,12 +1,20 @@
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.Date;
+import java.util.List;
+import java.util.logging.Level;
+
 import org.junit.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.logging.LogEntry;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+
+import static junit.framework.TestCase.assertTrue;
 
 
 public class StartBrowser {
@@ -33,6 +41,47 @@ public class StartBrowser {
             wait.until(ExpectedConditions.elementToBeClickable(By.id("activity_tutorial_close_button"))).click();
         }
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("bro_sentry_bar_fake")));
+
+        WebElement arrow = driver.findElement(By.id("bro_sentry_bar_fake_text"));
+        arrow.click();
+
+        WebElement arrowEdit = driver.findElement(By.id("bro_sentry_bar_input_edittext"));
+        arrowEdit.sendKeys("cat");
+
+        WebElement suggestN3 = driver.findElement(By.xpath("//*[@class='android.widget.RelativeLayout' and @index = '2']"));
+        suggestN3.click();
+
+        Date starttime = new Date();    //фиксирую время тапа
+        List<LogEntry> logEntryList;    //массив для хранения логов
+        boolean pageload = false;       //индикатор загрузки страницы
+
+        //проверяю логи на наличие события "url opened"
+        //Проверять буду 3 раза с интервалом 5 секунд
+        for(int i = 0; i < 3; i++) {
+
+            //Жду 5 секунд
+            WebDriverWait wd = new WebDriverWait(driver, 5);
+            try {
+                wd.until(ExpectedConditions.elementToBeClickable(By.id("NONEXISTENT ELEMENT")));
+            }
+            catch (Exception s){
+            }
+
+            //беру у драйвера логи
+            logEntryList = (List<LogEntry>) driver.manage().logs().get("logcat").filter(Level.ALL);
+
+            //Создаю обьект LogParser передаю в него массив логов и время тапа
+            LogParser lp = new LogParser(logEntryList, starttime);
+            //Запускаю поиск. если находим "url opened" то выходим
+            if(lp.FindStringInLog("url opened")){
+                i=3;
+                pageload =true;
+            }
+            else
+                i++;
+        }
+        //Проверка
+        assertTrue(pageload);
     }
 
     @After
